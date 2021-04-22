5f8884c29dfad343099009a92ab3626c92e1ac77
Riyaz226 <79141363+Riyaz226@users.noreply.github.com>
2021-04-22 20:18:34 +0530

    string document

---
+++ Javas.html/5.String Staement/main.js
@@ -0,0 +1,30 @@
+//string length
+//var txt="S.MOHAMED RIYAZ"
+//var sln=txt.length;
+//document.getElementById("rio").innerHTML=sln;
+
+//var ghy="I AM Go To Ooty,So I Travel In Train"
+//var gin=ghy.lastIndexOf("Ooty");
+//document.getElementById("rio").innerHTML=gin;
+
+//The slice() Method  
+//var ghy = "Car ,Bike ,Loory"
+//var res = ghy.slice(-8);
+//document.getElementById("rio").innerHTML=res;
+
+//The substring()Method
+//var ghy = "Car ,Bike ,Loory"
+//var res = ghy.substr(14);
+//document.getElementById("rio").innerHTML=res;
+
+//The concat()
+//var text1="S.Mohamed"
+//var text2="Riyaz"
+//var text3=text1.concat("",text2)
+//document.getElementById("rio").innerHTML=text3;
+
+//string.trim()
+//function  mydevlop() {
+//var str="   Hello Devlopers! "
+//alert( str.trim())
+//}
